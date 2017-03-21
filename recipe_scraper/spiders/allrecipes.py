0e60faf054611d1e74b4e5bd9562867e3fcbd245
CCInCharge <charles.l.chen.clc@gmail.com>
2017-03-20 22:53:37 -0700

    Modified allrecipes spider to inherit from SitemapSpider

--- recipe_scraper/spiders/allrecipes.py
+++ recipe_scraper/spiders/allrecipes.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spiders import SitemapSpider
 from recipe_scraper.items import RecipeItem
+from subprocess import check_output
 
-class AllrecipesSpider(scrapy.Spider):
+class AllrecipesSpider(SitemapSpider):
     name = "allrecipes"
-    start_urls = ['http://allrecipes.com/recipes/1947/everyday-cooking/quick-and-easy/']
+    gitBase = check_output('git rev-parse --show-toplevel', shell=True)
+    gitBase = gitBase.decode('utf-8').strip()
+    filenames = ['recipedetail.xml']
+    sitemap_urls = []
+    for file in filenames:
+        sitemap_urls.append('file://' + gitBase + "/recipe_scraper/spiders/allrecipesSitemap/" + file)
 
     def parse(self, response):
         pass
