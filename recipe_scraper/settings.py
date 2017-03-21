ef969bef985273ed4e789f0181c8e50db16c55e7
CCInCharge <charles.l.chen.clc@gmail.com>
2017-03-20 22:53:18 -0700

    Modified USER_AGENT

--- recipe_scraper/settings.py
+++ recipe_scraper/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'recipe_scraper.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'recipe_scraper (+http://www.yourdomain.com)'
+USER_AGENT = 'recipe_scraper (+https://github.com/CCInCharge/recipe-scraper)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
