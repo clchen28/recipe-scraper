3e7a44c48a18b5a4800ff01fd9530aebeecb58e2
CCInCharge <charles.l.chen.clc@gmail.com>
2017-03-29 22:12:39 -0700

    Added API endpoints to filter on prep time

--- app.py
+++ app.py
@@ -47,7 +47,63 @@ class searchRecipeTitle(Resource):
         # return JSON data (dict automatically converted to JSON)
         return response
 
-api.add_resource(searchRecipeTitle, '/search/title/<searchTerm>')
+class searchPrepTimeLessThan(Resource):
+    def get(self, searchTerm):
+        searchTerm = int(searchTerm)
+        # results will be an empty list if the search term is not found
+        results = \
+        session.query(Recipes).filter(Recipes.prepTime <= searchTerm).all()
+
+        # response is a dict which represents the API's response at this
+        # endpoint
+        response = {}
+        response["count"] = len(results)
+        response["recipes"] = []
+
+        for row in results:
+            rowData = {}
+            rowData["id"] = row.id
+            rowData["title"] = row.title
+            rowData["URL"] = row.URL
+            rowData["imgURL"] = row.imgURL
+            rowData["cuisine"] = row.cuisine
+            rowData["prepTime"] = row.prepTime
+            response["recipes"].append(rowData)
+
+        # return JSON data (dict automatically converted to JSON)
+        return response
+
+class searchPrepTimeBetween(Resource):
+    def get(self, low, high):
+        low = int(low)
+        high = int(high)
+        # results will be an empty list if the search term is not found
+        results = \
+        session.query(Recipes).filter(Recipes.prepTime >= low).\
+        filter(Recipes.prepTime <= high).all()
+
+        # response is a dict which represents the API's response at this
+        # endpoint
+        response = {}
+        response["count"] = len(results)
+        response["recipes"] = []
+
+        for row in results:
+            rowData = {}
+            rowData["id"] = row.id
+            rowData["title"] = row.title
+            rowData["URL"] = row.URL
+            rowData["imgURL"] = row.imgURL
+            rowData["cuisine"] = row.cuisine
+            rowData["prepTime"] = row.prepTime
+            response["recipes"].append(rowData)
+
+        # return JSON data (dict automatically converted to JSON)
+        return response
+
+api.add_resource(searchRecipeTitle, '/api/title/<searchTerm>')
+api.add_resource(searchPrepTimeLessThan, '/api/prepTime/lt/<searchTerm>')
+api.add_resource(searchPrepTimeBetween, '/api/prepTime/gt/<low>/lt/<high>')
 
 if __name__ == '__main__':
      app.run()
